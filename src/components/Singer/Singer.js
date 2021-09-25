70fc191b0b9bc2329a15325505f5bb4e2a778afd
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-09-26 01:04:55 +0600

    add fontawesome icons

--- src/components/Singer/Singer.js
+++ src/components/Singer/Singer.js
@@ -1,22 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Singer = (props) => {
   console.log(props.singerInfo);
   const { name, occupation, age, country, earnings, photo } = props.singerInfo;
   return (
     <div className="col-md-4 my-5">
-      <div class="card" style={{ width: "18rem" }}>
-        <img src={photo} class="card-img-top img-thumbnail" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{name}</h5>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={photo} className="card-img-top img-thumbnail" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
           <hr />
-          <p class="card-text">Occupation : {occupation}</p>
-          <p class="card-text">Age : {age}</p>
-          <p class="card-text">Country : {country}</p>
-          <p class="card-text">Earnings : $ {earnings}</p>
-          <a href="/" class="btn btn-primary">
-            Go somewhere
-          </a>
+          <p className="card-text">
+            <span className="fw-bold">Occupation :</span> {occupation}
+          </p>
+          <p className="card-text">
+            <span className="fw-bold">Age :</span> {age}
+          </p>
+          <p className="card-text">
+            <span className="fw-bold">Country :</span> {country}
+          </p>
+          <p className="card-text">
+            <span className="fw-bold">Earnings :</span> $ {earnings}
+          </p>
+          <button className="btn btn-secondary">
+            <FontAwesomeIcon className="me-2" icon={faShoppingCart} />
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
