b0fc0f4bf006f03b70b60b7da205da844074046f
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-09-26 21:15:14 +0600

    add icon to singer component

--- src/components/Singer/Singer.js
+++ src/components/Singer/Singer.js
@@ -1,33 +1,45 @@
 import React from "react";
 import "./Singer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faVideo,
+  faCameraRetro,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Singer = (props) => {
   // console.log(props.singerInfo);
-  const { name, occupation, age, country, earnings, photo } = props.singerInfo;
+  const { name, occupation, age, country, earnings, photo, genres } =
+    props.singerInfo;
   return (
     <div className="col-md-4 mb-5">
       <div className="card" style={{ width: "19rem" }}>
         <img src={photo} className="card-img-top img-thumbnail" alt="..." />
         <div className="card-body bg-light">
           <h5 className="card-title">{name}</h5>
+          <FontAwesomeIcon className="me-3 text-info" icon={faCameraRetro} />
+          <FontAwesomeIcon className="me-3 text-primary" icon={faMusic} />
+          <FontAwesomeIcon className="me-3 text-danger" icon={faVideo} />
           <hr />
-          <p className="card-text">
+          <p className="card-text mb-2">
             <span className="fw-bold">Occupation :</span> {occupation}
           </p>
-          <p className="card-text">
+          <p className="card-text mb-2">
             <span className="fw-bold">Age :</span> {age}
           </p>
-          <p className="card-text">
+          <p className="card-text mb-2">
             <span className="fw-bold">Country :</span> {country}
           </p>
-          <p className="card-text">
+          <p className="card-text mb-2">
+            <span className="fw-bold">Genres :</span> {genres}
+          </p>
+          <p className="card-text mb-3">
             <span className="fw-bold">Earnings :</span> $ {earnings}
           </p>
           <button
             onClick={() => props.handleAddToCart(props.singerInfo)}
-            className="btn btn-warning text-white"
+            className="btn btn-warning btn-sm text-white"
           >
             <FontAwesomeIcon className="me-2" icon={faShoppingCart} />
             Add to cart
--- src/singerData.js
+++ src/singerData.js
@@ -6,6 +6,7 @@ const singerData = [
     occupation: "Rapper & Song writer",
     country: "USA",
     earnings: 100000000,
+    genres: "Hip hop",
     photo:
       "https://styles.redditmedia.com/t5_4sae7p/styles/profileIcon_8rau8mmji0h71.jpg?width=256&height=256&crop=256:256,smart&s=c0c4c69fa4a06f416d9676a4f4321f873c96b3f9",
   },
@@ -16,6 +17,7 @@ const singerData = [
     occupation: "Singer",
     country: "Canada",
     earnings: 285000000,
+    genres: "Pop",
     photo:
       "https://pbs.twimg.com/profile_images/3683626156/0e3b06ea443b723647cc6dcdeab8262d.jpeg",
   },
@@ -26,6 +28,7 @@ const singerData = [
     occupation: "Singer & Actress",
     country: "USA",
     earnings: 330000000,
+    genres: "Rock",
     photo:
       "https://c-cl.cdn.smule.com/rs-s24/arr/9b/c4/0e4b601d-6177-47fa-86a4-f44d1e8b1da8.jpg",
   },
@@ -36,6 +39,7 @@ const singerData = [
     occupation: "Singer & Dancer",
     country: "Colombia",
     earnings: 300000000,
+    genres: "pop",
     photo:
       "https://pbs.twimg.com/profile_images/3243645236/d260be66e1665b58593c44f26a64acba.jpeg",
   },
@@ -46,6 +50,7 @@ const singerData = [
     occupation: "Singer & Song writer",
     country: "USA",
     earnings: 850000000,
+    genres: "electronica",
     photo:
       "https://pbs.twimg.com/profile_images/711390095543246852/U6X2rSAd_400x400.jpg",
   },
@@ -56,6 +61,7 @@ const singerData = [
     occupation: "Singer & Actor",
     country: "England",
     earnings: 16421989,
+    genres: "Britpop",
     photo:
       "https://i.pinimg.com/550x/40/dc/0d/40dc0d9042155cf8d87470ae034820c6.jpg",
   },
@@ -66,6 +72,7 @@ const singerData = [
     occupation: "Singer & Actor",
     country: "India",
     earnings: 50000000,
+    genres: "Classical Music",
     photo:
       "https://c-cl.cdn.smule.com/s-sf-bck2/arr/8d/df/65d4aab5-64ad-414b-a9ae-12a3ade7d97d.jpg",
   },
@@ -76,6 +83,7 @@ const singerData = [
     occupation: "Singer & Actress",
     country: "Bangladesh",
     earnings: 72800,
+    genres: "Ghazal",
     photo:
       "https://www.suneeshsundar.com/wp-content/uploads/2018/05/runa-laila1.jpg",
   },
@@ -86,6 +94,7 @@ const singerData = [
     occupation: "Singer & Musician",
     country: "Pakistan",
     earnings: 218000,
+    genres: "Qawwali",
     photo:
       "https://c-cl.cdn.smule.com/rs-s-sf-2/sing_google/performance/cover/58/77/9b0dbb57-261c-4cdf-a149-22b53f6a51d7.jpg",
   },
@@ -96,6 +105,7 @@ const singerData = [
     occupation: "Singer & Songwriter",
     country: "Japan",
     earnings: 209000,
+    genres: "Soul jazz",
     photo:
       "https://c-cl.cdn.smule.com/rs-s31/arr/e4/e2/8c598d7b-ccaa-431a-93ce-969641ad4541.jpg",
   },
@@ -106,6 +116,7 @@ const singerData = [
     occupation: "Singer & Model",
     country: "Australia",
     earnings: 4500000,
+    genres: "R&B",
     photo:
       "https://pbs.twimg.com/profile_images/2837860302/6c67fbe16f17f8da8f161100fcd4c9e3.jpeg",
   },
@@ -116,6 +127,7 @@ const singerData = [
     occupation: "Singer & Actress",
     country: "USA",
     earnings: 42000000,
+    genres: "Hip hop",
     photo:
       "https://styles.redditmedia.com/t5_2t3f4/styles/communityIcon_7m4o7m71dou51.png",
   },
